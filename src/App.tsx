136f82f8ba0b39a85e00021dd8af55732e7a8827
evertonlsds <everton0102@gmail.com>
2023-04-12 18:36:06 -0300

    test buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secundary" />
       <Button variant="danger" />
       <Button variant="success" />
-    </>
+    </ThemeProvider>
   );
 }
