0ff6a514f4f4363b78adfe1a272733e9825bf4fd
evertonlsds <everton0102@gmail.com>
2023-05-09 07:45:32 -0300

    new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secundary" />
-      <Button variant="danger" />
-      <Button variant="success" />
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
